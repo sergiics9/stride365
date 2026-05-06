80004d39a6e5438270fad2fa3ee1aa854e6d73b1
Sergi <sergi20031005@gmail.com>
2026-05-06 13:23:49 +0200

    Add configuration file for Stripe payment settings

---
+++ backend/config/stripe.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+
+
+    'prices' => [
+        'club' => env('STRIPE_PRICE_CLUB', 'price_REPLACE_CLUB'),
+        'socio' => env('STRIPE_PRICE_SOCIO', 'price_REPLACE_SOCIO'),
+    ],
+
+    'amounts' => [
+        'club' => '129.99',
+        'socio' => '39.99',
+    ],
+
+    'currency' => 'usd',
+
+];
