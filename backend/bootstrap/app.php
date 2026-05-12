635e04dcef7f7322be0c1928fb139b97d2a4fb82
sergiics9 <sergi20031005@gmail.com>
2026-05-12 17:48:08 +0200

    add CORS configuration and update middleware for API requests

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -10,9 +10,9 @@ use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         apiPrefix: 'api',
     )
@@ -28,7 +28,14 @@ return Application::configure(basePath: dirname(__DIR__))
             'webhook/stripe',
             'api/webhook/stripe',
         ]);
+
+        // CORS
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(function ($request) {
+            return $request->is('api/*') || $request->expectsJson();
+        });
     })->create();
